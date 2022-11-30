89b83ad6a4513fcc7bae80e805d122f3410b9ed7
Derrick Jamal <jamalderrick6@gmail.com>
2022-11-30 19:57:20 +0300

    fix: Add mongo cluster to .env file

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 // Replace this with your MONGOURI.
 const MONGOURI =
-  `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@comparocluster.pgf9jsw.mongodb.net/${process.env.MONGODB_DATABASE_NAME}?retryWrites=true&w=majority`;
+  `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGO_CLUSTER}/${process.env.MONGODB_DATABASE_NAME}?retryWrites=true&w=majority`;
 console.log("mongo url", MONGOURI)
 const InitiateMongoServer = async () => {
   try {
